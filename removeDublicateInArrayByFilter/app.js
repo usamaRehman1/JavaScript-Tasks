4c3df55c3bb32cb4649ae347eb1c8cb468641958
Muhammad Usama <46845901+usamaRehman1@users.noreply.github.com>
2024-03-02 05:11:36 -0800

    remove dublicate update

--- removeDublicateInArrayByFilter/app.js
+++ removeDublicateInArrayByFilter/app.js
@@ -18,3 +18,38 @@ function removedublicatebyset(data) {
     return [...new Set(data)]
 }
 console.log(removedublicatebyset(fruits))
+
+// remove dublicate by using condition
+function remove_dublicate(data) {
+    let removeDublicateArr = [];
+    let dublicateElementArr = [];
+    let flag = false;
+
+    for (let i = 0; i < data.length; i++) {
+        var previousElement = data[i];
+        flag = false;
+        for (let j = i+1; j < data.length; j++) {
+            if (data[j] === previousElement) {
+                flag = true;
+                var dublicate = data[j];
+            }
+        }
+
+        if (flag) {
+            if (dublicateElementArr.length !== 0 ) {
+                if (dublicateElementArr.indexOf(dublicate) == -1) {
+                    dublicateElementArr.push(dublicate)
+                }
+            }else{
+                dublicateElementArr.push(dublicate);
+            }
+        }else{
+            removeDublicateArr.push(data[i]);
+        }
+    }
+
+    console.log(removeDublicateArr)
+    console.log(dublicateElementArr)
+}
+
+remove_dublicate(fruits)
